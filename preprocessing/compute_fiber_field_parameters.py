fad30b16ad6de06e95439e2425a0212b75be0241
Elisabeth Brockhaus <elisabeth.brockhaus@quantco.com>
2024-10-04 12:59:18 +0200

    Plot a few of the UAC-based coordinates

--- preprocessing/compute_fiber_field_parameters.py
+++ preprocessing/compute_fiber_field_parameters.py
@@ -1,5 +1,9 @@
 # %%
+import matplotlib.pyplot as plt
+import numpy as np
+
 from prior_fields.tensor.io import read_meshes_from_lge_mri_data
+from prior_fields.tensor.plots import add_3d_vectors_to_plot
 from prior_fields.tensor.vector_heat_method import get_uac_basis_vectors
 
 # %%
@@ -9,5 +13,27 @@ V, F, uac, fibers = read_meshes_from_lge_mri_data()
 directions_constant_alpha, directions_constant_beta = get_uac_basis_vectors(
     V[0], F[0], uac[0]
 )
+
+# %%
+s = np.random.randint(0, V[0].shape[0] - 20)
+
+fig = plt.figure(figsize=(10, 10))
+ax = fig.add_subplot(projection="3d")
+ax = plt.gca()
+ax.set_aspect("equal")
+
+add_3d_vectors_to_plot(
+    V[0][s : (s + 20)], directions_constant_alpha[s : (s + 20)], ax, length=100, lw=1
+)
+add_3d_vectors_to_plot(
+    V[0][s : (s + 20)],
+    directions_constant_beta[s : (s + 20)],
+    ax,
+    length=100,
+    lw=1,
+    color="tab:orange",
+)
+
+plt.show()
 
 # %%
