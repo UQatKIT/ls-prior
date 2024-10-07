a4e82cbfeae20f796d533c9b7d263cf6cc11a12f
Elisabeth Brockhaus <elisabeth.brockhaus@quantco.com>
2024-10-07 10:51:49 +0200

    Add plots with fibers in UAC unit square and anatomical structures

--- preprocessing/compute_fiber_field_parameters.py
+++ preprocessing/compute_fiber_field_parameters.py
@@ -3,19 +3,16 @@ from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.stats import circmean, circstd, mode
 
-from prior_fields.tensor.io import (
-    get_mesh_and_point_data_from_lge_mri_based_data,
-)
-from prior_fields.tensor.mapper import (
-    get_coefficients,
-    map_fibers_to_tangent_space,
-)
+from prior_fields.tensor.io import get_mesh_and_point_data_from_lge_mri_based_data
+from prior_fields.tensor.mapper import get_coefficients, map_fibers_to_tangent_space
 from prior_fields.tensor.plots import add_3d_vectors_to_plot
+from prior_fields.tensor.transformer import vector_coefficients_2d_to_angles
 from prior_fields.tensor.vector_heat_method import get_uac_basis_vectors
 
 # %%
-V, F, uac, fibers = get_mesh_and_point_data_from_lge_mri_based_data(
+V, F, uac, fibers, tag = get_mesh_and_point_data_from_lge_mri_based_data(
     Path("data/LGE-MRI-based/A")
 )
 
@@ -103,12 +100,14 @@ fig, ax = plt.subplots(1, 1, figsize=(8, 8))
 ax.set_aspect("equal")
 c = ax.scatter(uac[:, 0], uac[:, 1], c=fiber_coeffs_x1, s=0.5)
 fig.colorbar(c)
+plt.title("alpha-coefficients of fibers in UAC basis")
 plt.show()
 
 fig, ax = plt.subplots(1, 1, figsize=(8, 8))
 ax.set_aspect("equal")
 c = ax.scatter(uac[:, 0], uac[:, 1], c=fiber_coeffs_x2, s=0.5)
 fig.colorbar(c)
+plt.title("beta-coefficients of fibers in UAC basis")
 plt.show()
 
 fig, ax = plt.subplots(1, 1, figsize=(8, 8))
@@ -123,5 +122,82 @@ ax.quiver(
     scale=80,
     width=0.001,
 )
+plt.title("Fibers mapped to UAC")
 plt.savefig("figures/uac_fibers.svg")
+plt.show()
+
+# %%
+# Get fiber angle within (-pi, pi] in UAC system
+phi = vector_coefficients_2d_to_angles(fiber_coeffs_x1, fiber_coeffs_x2)
+
+# %%
+# Split UAC unit square into grid and compute component-wise parameters (fibers/tags)
+h = 100
+grid = np.linspace(0, 1, h, endpoint=True)
+grid[-1] += 1e-6  # include alpha, beta = 1
+fiber_coeff_mean_x1 = np.zeros((h, h))
+fiber_coeff_mean_x2 = np.zeros((h, h))
+phi_circmean = np.zeros((h, h))
+phi_circstd = np.zeros((h, h))
+tag_mode = np.zeros((h, h))
+
+for i in range(len(grid) - 1):
+    for j in range(len(grid) - 1):
+        mask = (
+            (uac[:, 0] >= grid[i])
+            & (uac[:, 0] < grid[i + 1])
+            & (uac[:, 1] >= grid[j])
+            & (uac[:, 1] < grid[j + 1])
+        )
+        fiber_coeff_mean_x1[j, i] = fiber_coeffs_x1[mask].mean()
+        fiber_coeff_mean_x2[j, i] = fiber_coeffs_x2[mask].mean()
+        phi_circmean[j, i] = circmean(phi[mask])
+        phi_circstd[j, i] = circstd(phi[mask])
+        tag_mode[j, i] = mode(tag[mask]).mode
+
+fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+ax.set_aspect("equal")
+plt.scatter(*np.meshgrid(grid, grid), c=tag_mode, s=10, marker="s", alpha=0.3)
+ax.quiver(
+    *np.meshgrid(grid, grid),
+    fiber_coeff_mean_x1,
+    fiber_coeff_mean_x2,
+    angles="xy",
+    scale_units="xy",
+    scale=80,
+    width=0.001,
+)
+plt.title("Fibers in UAC over grid with anatomical structures")
+plt.savefig("figures/uac_fibers_discretized.svg")
+plt.show()
+
+# %%
+fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+ax.set_aspect("equal")
+plt.scatter(*np.meshgrid(grid, grid), c=phi_circmean, s=10, marker="s", alpha=0.3)
+ax.quiver(
+    *np.meshgrid(grid, grid),
+    fiber_coeff_mean_x1,
+    fiber_coeff_mean_x2,
+    angles="xy",
+    scale_units="xy",
+    scale=80,
+    width=0.001,
+)
+plt.title("Circular mean of fiber angle")
+plt.show()
+
+fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+ax.set_aspect("equal")
+plt.scatter(*np.meshgrid(grid, grid), c=phi_circstd, s=10, marker="s", alpha=0.3)
+ax.quiver(
+    *np.meshgrid(grid, grid),
+    fiber_coeff_mean_x1,
+    fiber_coeff_mean_x2,
+    angles="xy",
+    scale_units="xy",
+    scale=80,
+    width=0.001,
+)
+plt.title("Circular standard deviation of fiber angle")
 plt.show()
