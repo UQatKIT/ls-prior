1bf2f82dc394c0f537b127529aa0670ea18e7d60
Elisabeth Brockhaus <elisabeth.brockhaus@quantco.com>
2024-10-18 18:13:55 +0200

    Merge data from different geometries earlier

--- preprocessing/compute_fiber_field_parameters.py
+++ preprocessing/compute_fiber_field_parameters.py
@@ -14,51 +14,51 @@ from prior_fields.tensor.vector_heat_method import get_uac_basis_vectors
 
 # %%
 # takes about 70 seconds
-V, F, uac, fibers, tags = read_meshes_from_lge_mri_data()
+V_dict, F_dict, uac_dict, fibers_dict, tags_dict = read_meshes_from_lge_mri_data()
+keys = sorted(V_dict.keys())
 
 # %%
-# takes about 90 seconds
-fibers_in_tangent_space: dict[int, ArrayNx3] = dict()
-fiber_coeffs_x: dict[int, ArrayNx3] = dict()
-fiber_coeffs_y: dict[int, ArrayNx3] = dict()
-fiber_angles: dict[int, ArrayNx3] = dict()
+# takes about 80 seconds
+directions_constant_beta_dict: dict[int, ArrayNx3] = dict()
+directions_constant_alpha_dict: dict[int, ArrayNx3] = dict()
 
-for i in sorted(V.keys()):
-    print(f"Geometry {i}:")
-    print("Get UAC-based tangent space coordinates.")
-    directions_constant_beta, directions_constant_alpha = get_uac_basis_vectors(
-        V[i], F[i], uac[i]
-    )
-    print("Map fibers to tangent space.")
-    fibers_in_tangent_space[i] = map_fibers_to_tangent_space(
-        fibers[i], directions_constant_beta, directions_constant_alpha
-    )
-    print("Get coefficients of fibers in tangent space coordinates.")
-    fiber_coeffs_x[i], fiber_coeffs_y[i] = get_coefficients(
-        fibers_in_tangent_space[i], directions_constant_beta, directions_constant_alpha
-    )
-    print("Get fiber angle within (-pi/2, pi/2] in UAC system.")
-    fiber_angles[i] = vector_coefficients_2d_to_angles(
-        fiber_coeffs_x[i], fiber_coeffs_y[i]
+for i in keys:
+    print(f"Geometry {i}: Get UAC-based tangent space coordinates.")
+    directions_constant_beta_dict[i], directions_constant_alpha_dict[i] = (
+        get_uac_basis_vectors(V_dict[i], F_dict[i], uac_dict[i])
     )
     print()
 
 # %%
-# Collect data from different geometries in single array for mean computation
-uac_array = np.vstack([uac[i] for i in np.arange(1, 8)])
-fiber_coeffs_x_array = np.hstack([fiber_coeffs_x[i] for i in np.arange(1, 8)])
-fiber_coeffs_y_array = np.hstack([fiber_coeffs_y[i] for i in np.arange(1, 8)])
-fiber_angles_array = np.hstack([fiber_angles[i] for i in np.arange(1, 8)])
-tags_array = np.hstack([tags[i] for i in np.arange(1, 8)])
+# Unite different geometries
+uac = np.vstack([uac_dict[i] for i in keys])
+fibers = np.vstack([fibers_dict[i] for i in keys])
+tags = np.hstack([tags_dict[i] for i in keys])
+directions_constant_beta = np.vstack([directions_constant_beta_dict[i] for i in keys])
+directions_constant_alpha = np.vstack([directions_constant_alpha_dict[i] for i in keys])
+
+# %%
+# Map fibers to tangent space
+fibers_in_tangent_space = map_fibers_to_tangent_space(
+    fibers, directions_constant_beta, directions_constant_alpha
+)
+
+# Get coefficients of fibers in tangent space coordinates
+fiber_coeffs_x, fiber_coeffs_y = get_coefficients(
+    fibers_in_tangent_space, directions_constant_beta, directions_constant_alpha
+)
+
+# Get fiber angle within (-pi/2, pi/2] in UAC system
+fiber_angles = vector_coefficients_2d_to_angles(fiber_coeffs_x, fiber_coeffs_y)
 
 # %%
 # takes about 30 seconds
 FiberGridComputer(
-    uac=uac_array,
-    fiber_coeffs_x=fiber_coeffs_x_array,
-    fiber_coeffs_y=fiber_coeffs_y_array,
-    fiber_angles=fiber_angles_array,
-    anatomical_structure_tags=tags_array,
+    uac=uac,
+    fiber_coeffs_x=fiber_coeffs_x,
+    fiber_coeffs_y=fiber_coeffs_y,
+    fiber_angles=fiber_angles,
+    anatomical_structure_tags=tags,
     max_depth=7,
     point_threshold=100,
 ).get_fiber_grid().save()
