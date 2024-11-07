729f3afd629732435e4505fa899ecd10e1b41dcc
Elisabeth Brockhaus <elisabeth.brockhaus@quantco.com>
2024-11-07 08:35:52 +0100

    Add stats regarding BiLaplacianPrior

--- prior_fields/tensor/scripts/compute_stats.py
+++ prior_fields/tensor/scripts/compute_stats.py
@@ -1,12 +1,15 @@
 # %%
 from collections.abc import Callable
 from datetime import datetime
+from pathlib import Path
 
 import numpy as np
 
+from prior_fields.prior.converter import create_triangle_mesh_from_coordinates
 from prior_fields.prior.dtypes import ArrayNx3
+from prior_fields.prior.prior import BiLaplacianPrior, BiLaplacianPriorNumpyWrapper
 from prior_fields.tensor.fiber_grid import get_fiber_parameters_from_uac_data
-from prior_fields.tensor.parameters import Geometry
+from prior_fields.tensor.parameters import Geometry, PriorParameters
 from prior_fields.tensor.reader import (
     _map_fibers_and_tags_to_vertices,
     read_all_human_atrial_fiber_meshes,
@@ -23,13 +26,13 @@ def profile(f: Callable, **kwargs):
     return duration.seconds + duration.microseconds * 1e-6
 
 
+V_dict, F_dict, uac_dict, fibers_dict, tags_dict = read_all_human_atrial_fiber_meshes()
+keys = sorted(V_dict.keys())
+
 # %%
 ################################
 # UAC-based coordinate systems #
 ################################
-V_dict, F_dict, uac_dict, fibers_dict, tags_dict = read_all_human_atrial_fiber_meshes()
-keys = sorted(V_dict.keys())
-
 directions_constant_beta_dict: dict[int, ArrayNx3] = dict()
 directions_constant_alpha_dict: dict[int, ArrayNx3] = dict()
 
@@ -73,6 +76,9 @@ print(
 )
 
 # %%
+###################################
+# Compute parameters for geometry #
+###################################
 durations_compute_fiber_paramters: list[float] = []
 for i in keys:
     durations_compute_fiber_paramters.append(
@@ -87,6 +93,82 @@ print("Stats for computing fiber parameters for the 7 geometries:")
 print(
     f"mean={np.mean(durations_compute_fiber_paramters):.2f}s, "
     f"std={np.std(durations_compute_fiber_paramters):.2f}s"
+)
+
+# %%
+##################################
+# Initialize bi-Laplacian priors #
+##################################
+duration_prior_init = profile(
+    BiLaplacianPriorNumpyWrapper,
+    **dict(V=V_dict[3], F=F_dict[3], sigma=0.2, ell=0.1, seed=1),
+)
+
+print(
+    "Duration for initializing a baseline prior (wrapper) "
+    f"on geometry 3: {duration_prior_init:.2f}"
+)
+
+mesh3 = create_triangle_mesh_from_coordinates(V_dict[3], F_dict[3])
+duration_prior_init = profile(
+    BiLaplacianPrior,
+    **dict(mesh=mesh3, sigma=0.2, ell=0.1, seed=1),
+)
+
+print(
+    "Duration for initializing a baseline prior (no wrapper) "
+    f"on geometry 3: {duration_prior_init:.2f}"
+)
+
+params = PriorParameters.load(Path("data/parameters/params_3.npy"))
+duration_prior_init = profile(
+    BiLaplacianPriorNumpyWrapper,
+    **dict(
+        V=V_dict[3],
+        F=F_dict[3],
+        sigma=params.sigma,
+        ell=params.ell,
+        mean=params.mean,
+        seed=1,
+    ),
+)
+
+print(
+    "Duration for initializing a data-informed prior "
+    f"on geometry 3: {duration_prior_init:.2f}"
+)
+
+# %%
+###################################
+# Sample from bi-Laplacian priors #
+###################################
+prior = BiLaplacianPriorNumpyWrapper(
+    V=V_dict[3], F=F_dict[3], sigma=0.2, ell=0.1, seed=1
+)
+
+durations_prior_sample = [profile(prior.sample) for _ in range(100)]
+print(
+    "Duration for sampling from the baseline prior:\n"
+    f"First sample: {durations_prior_sample[0]:.2f}\n"
+    f"Further samples: {np.mean(durations_prior_sample[1:]):.2f} "
+    f"({np.std(durations_prior_sample[1:]):.2f})"
+)
+
+prior = BiLaplacianPriorNumpyWrapper(
+    V=V_dict[3],
+    F=F_dict[3],
+    sigma=params.sigma,
+    ell=params.ell,
+    mean=params.mean,
+    seed=1,
+)
+
+durations_prior_sample = [profile(prior.sample) for _ in range(100)]
+print(
+    "Duration for sampling from the data-informed prior:\n"
+    f"First sample: {durations_prior_sample[0]:.2f}\n"
+    f"Further samples: {np.mean(durations_prior_sample[1:]):.2f} "
+    f"({np.std(durations_prior_sample[1:]):.2f})"
 )
 
 # %%
